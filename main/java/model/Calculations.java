9754b2fa49c7c64cea631abad62f0aebe477c761
Winniebob <vilksmax@gmail.com>
2023-12-02 19:51:00 +0300

    Первый коммит

---
+++ main/java/model/Calculations.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Calculations {
+
+    public int sum (int a, int b){
+        return a + b;
+    }
+
+    public int dif (int a, int b){
+        return a - b;
+    }
+
+    public int div (int a, int b){
+        return a/b;
+    }
+
+    public int mul(int a, int b){
+        return a*b;
+    }
+
+}
